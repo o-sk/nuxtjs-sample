2134d350b96d01dff33ba3d7c6979182e668b0c3
o-sk <m.ohsuka@gmail.com>
2019-05-04 23:11:13 +0900

    Use titleTemplate

--- pages/users/_id.vue
+++ pages/users/_id.vue
@@ -18,6 +18,11 @@
 
 <script>
 export default {
+  head() {
+    return {
+      title: `${this.user.id}'s page`
+    }
+  },
   async asyncData({ route, app }) {
     const user = await app.$axios.$get(`https://qiita.com/api/v2/users/${route.params.id}`)
     const items = await app.$axios.$get(`https://qiita.com/api/v2/items?query=user:${route.params.id}`)
